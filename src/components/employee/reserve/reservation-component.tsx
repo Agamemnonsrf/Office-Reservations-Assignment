2a58a66ebf614103a71009109497e721a3ca6cdd
Ooey <agiospantas@gmail.com>
2024-01-08 23:02:25 +0200

    Remove unused import in reservation-component.tsx

--- src/components/employee/reserve/reservation-component.tsx
+++ src/components/employee/reserve/reservation-component.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Fragment } from "react";
+import { useState, useEffect } from "react";
 import BuildingCard from "./building-card";
 import Filters from "./filters";
 import {
