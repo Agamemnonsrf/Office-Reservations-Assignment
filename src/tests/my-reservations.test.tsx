7cff3edd7c40ec4f7b13948fdef6aa965eedbad3
Aghs <agiospantas@gmail.com>
2024-02-02 13:20:31 +0200

    added few more tests

--- src/tests/my-reservations.test.tsx
+++ src/tests/my-reservations.test.tsx
@@ -1,10 +1,33 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import MyReservations from "../routes/my-reservations";
 import "@testing-library/jest-dom";
+import RootElement from "../main";
 
 describe(MyReservations, () => {
     it("should render the My Reservations screen", () => {
         const { getByText } = render(<MyReservations />);
         expect(getByText("My Reservations")).toBeInTheDocument();
+    });
+
+    it("should make a new reservation appear in the list", () => {
+        const { getByRole, getByText, getByTestId } = render(<RootElement />);
+        const loginButton = getByRole("button", { name: /login/i });
+        const loginDropdown = getByText("Select User");
+
+        fireEvent.click(loginDropdown);
+        const user = getByText("John Doe");
+        fireEvent.click(user);
+        fireEvent.click(loginButton);
+        fireEvent.click(getByText("Reserve"));
+        const dateInput = getByTestId("date") as HTMLInputElement;
+        fireEvent.change(dateInput, { target: { value: "2024-01-31" } });
+        fireEvent.click(getByText("Building A"));
+        fireEvent.click(getByText("Room 1"));
+        fireEvent.click(getByText("Workspace 1-1"));
+        fireEvent.click(getByText("Set Workspaces"));
+        fireEvent.click(getByText("Make Reservation"));
+        fireEvent.click(getByText("Confirm"));
+        fireEvent.click(getByText("My Reservations"));
+        expect(getByText("Wed Jan 31 2024 (Passed)")).toBeInTheDocument();
     });
 });
--- src/tests/reservation-component.test.tsx
+++ src/tests/reservation-component.test.tsx
@@ -1,6 +1,7 @@
 import { render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ReservationComponent from "../components/employee/reserve/reservation-component";
+import RootElement from "../main";
 
 describe(ReservationComponent, () => {
     it("should render the reservation component", () => {
@@ -20,5 +21,26 @@ describe(ReservationComponent, () => {
         fireEvent.change(dateInput, { target: { value: "2024-01-31" } });
 
         expect(getByText("Building A")).toBeInTheDocument();
+    });
+    global.alert = jest.fn();
+    it("should be able to make a reservation", () => {
+        const { getByRole, getByText, getByTestId } = render(<RootElement />);
+        const loginButton = getByRole("button", { name: /login/i });
+        const loginDropdown = getByText("Select User");
+
+        fireEvent.click(loginDropdown);
+        const user = getByText("John Doe");
+        fireEvent.click(user);
+        fireEvent.click(loginButton);
+        fireEvent.click(getByText("Reserve"));
+        const dateInput = getByTestId("date") as HTMLInputElement;
+        fireEvent.change(dateInput, { target: { value: "2024-01-31" } });
+        fireEvent.click(getByText("Building A"));
+        fireEvent.click(getByText("Room 1"));
+        fireEvent.click(getByText("Workspace 1-1"));
+        fireEvent.click(getByText("Set Workspaces"));
+        fireEvent.click(getByText("Make Reservation"));
+        fireEvent.click(getByText("Confirm"));
+        expect(window.alert).toHaveBeenCalledWith("Reservation added!");
     });
 });
